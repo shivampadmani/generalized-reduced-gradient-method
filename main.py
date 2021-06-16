39995b70895410f0a5e12523ea537073d5231b36
Shivam Padmani <56795691+shivampadmani@users.noreply.github.com>
2021-06-16 12:19:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,9 +26,11 @@ if __name__ == '__main__':
         delfuny = Derivative(objfun,y).doit()
         templist = [[delfunx.subs([(x, x0), (y, y0)]),delfuny.subs([(x, x0), (y, y0)])],]  # List of list of gradients of obj function
         rows, cols = (10, 2)
-        nextvalue = [[0,0]] * 10#[[0 for i in range(cols)] for j in range(rows)]
+        nextvalue = [[0,0]] * 100#[[0 for i in range(cols)] for j in range(rows)]
         h = symbols('h')
-        for iteration_no in range(0,10): #assumed 10 iteration required taking index 0 to 9
+        k = 1
+        iteration_no = 0
+        while k == 1: #assumed 10 iteration required taking index 0 to 9
             print("Iteration No. ",iteration_no+1)
             # time.sleep(1)
             nextvalue[iteration_no][0] = itervalue[iteration_no][0] + h * templist[iteration_no][0]
@@ -36,7 +38,11 @@ if __name__ == '__main__':
             step_size = newton(nextvalue[iteration_no],objfun) #stepsize of iteration
             itervalue.append([nextvalue[iteration_no][0].subs(h, step_size), nextvalue[iteration_no][1].subs(h, step_size)])
             templist.append([delfunx.subs(x,itervalue[iteration_no][0]),delfuny.subs(y,itervalue[iteration_no][1])])
+            if templist[iteration_no][0] == 0 and templist[iteration_no][1] == 0:
+                print("Solution obtained:")
+                break
             # print(f"Step size (h) for iteration no. {iteration_no+1} is {step_size}")
             # print("itervalue" , itervalue)
             # print("templist",templist)
+            iteration_no += 1
         print(nextvalue[-1][0].subs(h,step_size),nextvalue[-1][1].subs(h,step_size))
